67538073f032df8e5df178757a8b31a1a889445e
MikealFowler <mikeyfowler36@gmail.com>
2025-04-01 14:41:43 -0300

    added coments for my code

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,9 @@ function App() {
   const [player, setPlayer] = useState(null);
   const [guess, setGuess] = useState('');
   const [hint, setHint] = useState(false);
-  
+  const [allPlayers, setAllPlayers] = useState(null);
 
+  // This function is for grabbing a new random player from the database
   const grabNewPlayer = () => {
     api.get('/randomplayer')
       .then((res) => {
@@ -18,11 +19,24 @@ function App() {
       });
   }
 
+  // This function is for grabbing all players from the database
+  const grabAllPlayers = () => {
+    api.get('/allplayers')
+      .then((res) => {
+        setAllPlayers(res.data);
+      })
+      .catch((err) => {
+        console.error('API error:', err);
+      });
+  }
+
+  // this useEffect is to grab a new random player, and all players upon loading the website
   useEffect(() => {
     grabNewPlayer();
+    grabAllPlayers();
   }, []);
 
-
+  // this component is for handling the guess box to see if its the correct player or not
   const handleGuess = (e) => {
     e.preventDefault();
     if (!player) return;
@@ -39,6 +53,7 @@ function App() {
     }
     setGuess('');
   };
+  // This is to toggle the hint box on or off
   const showhint = () => {
     setHint(!hint)
   }
@@ -46,22 +61,24 @@ function App() {
   return (
     <div>
       <h2 className='title'>NBA 75th Anniversary Team </h2>
-      <section className='centered-box'>
-        <p className='listOfPlayers'>LeBron James, Magic Johnson, Reggie Miller, David Robinson, Damian Lillard, Chris Paul, Robert Parish, James Harden,
-          Bob Pettit, Rick Barry, Jerry Lucas, Elgin Baylor, Ray Allen, Russell Westbrook, Bill Walton, Bill Russell, John Havlicek, Clyde Drexler, Earl Monroe,
-          Isiah Thomas, Larry Bird, Karl Malone, Dominique Wilkins, George Mikan, Patrick Ewing, Allen Iverson, Moses Malone, Julius Erving, John Stockton,
-          Nate Archibald, Steve Nash, Kevin Garnett, Kevin McHale, Bob Cousy, Gary Payton, Bob McAdoo, Anthony Davis, Pete Maravich, James Worthy,
-          Shaquille O’Neal, Tim Duncan, Lenny Wilkens, Scottie Pippen, Kevin Durant, Dwyane Wade, Jerry West, Hal Greer, Kawhi Leonard, Kareem Abdul-Jabbar, Carmelo Anthony,
-          Paul Pierce, Rick Barry, George Gervin, Dennis Rodman, Giannis Antetokounmpo, Dave Bing, Billy Cunningham, Wilt Chamberlain, Wes Unseld, Dave Cowens, Michael Jordan,
-          Dolph Schayes, Nate Thurmond, Dave DeBusschere, Bill Sharman, Dirk Nowitzki, Bill Russell, Sam Jones, Paul Arizin, Elvin Hayes, Oscar Robertson, Walt Frazier,
-          Charles Barkley, Larry Bird, Bob Pettit, Stephen Curry, Kobe Bryant, John Stockton, Karl Malone, Jerry Lucas, Magic Johnson</p>
+      <section className='listOfPlayers'>
+        {allPlayers ? (
+          <p>
+            {allPlayers.map((player) => {
+              const name = player.player_name;
+              return `${name + ", "}`;
+            })}
+          </p>
+        ) : (
+          <p>Loading player names...</p>
+        )}
       </section>
-
       <h1>Guess That NBA Player</h1>
       {player ? (
         <div className='player-stats'>
           <ul>
-            {/* <p className='stat'><strong>Name:</strong> {player.player_name}</p> */}
+            {/* This is line of code below reveals the players name with rest of stats was  used for developemnt 
+            <p className='stat'><strong>Name:</strong> {player.player_name}</p> */}
             <p className='stat'><strong>Points Avg:</strong> {player.points_avg}</p>
             <p className='stat'><strong>Assists Avg:</strong> {player.assists_avg}</p>
             <p className='stat'><strong>Rebounds Avg:</strong> {player.rebounds_avg}</p>
@@ -70,6 +87,7 @@ function App() {
             <p className='stat'><strong>Finals Appearence:</strong> {player.finals_played}</p>
             <p className='stat'><strong>Finals Won:</strong> {player.finals_won}</p>
           </ul>
+          {/* This is my hint logic to give just the initials back of the Basketball player */}
           {hint && (
             <p className='hint-answer'>
               This player’s initials are:{' '}
@@ -83,6 +101,7 @@ function App() {
       ) : (
         <p>Loading player...</p>
       )}
+      {/* This is where the code handles clicking the button to submit guess */}
       <form onSubmit={handleGuess}>
         <input
           type="text"
